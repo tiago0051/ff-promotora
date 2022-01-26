ee832f8a42e789f51db83f464a926de091cce755
Tiago Salgado <tiagosalgado2002@hotmail.com>
2022-01-26 17:41:32 -0300

    Atualização de informações

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import {FaWhatsapp} from "react-icons/fa";
 
 import style from './style.module.css'
@@ -13,7 +14,8 @@ export default function Footer() {
             <div>
                 <h2>Precisa de ajuda?</h2>
                 <button><FaWhatsapp/>FALE COM A GENTE</button>
-                <p>Fale com a gente no whatsapp. Nossa equipe de atendimento está disponível para te atender de segunda a sexta, de 10 ás 16H</p>
+                <p>Fale com a gente no whatsapp. Nossa equipe de atendimento está disponível para te atender de segunda a sexta, de 9 ás 18H</p>
+                <span>Desenvolvido por <Link href="https://api.whatsapp.com/send/?phone=5521982436773&text&app_absent=0" passHref>Conect-se</Link></span>
             </div>
         </footer>
     )
