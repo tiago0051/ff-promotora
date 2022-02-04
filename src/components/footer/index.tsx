e928121fc3ef319c7ef85ad85489ee6e149f89ef
Tiago Salgado <tiagosalgado2002@hotmail.com>
2022-02-04 18:49:43 -0300

    Correção layout

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
 import {FaWhatsapp} from "react-icons/fa";
+import { useRouter } from "next/router";
 
 import style from './style.module.css'
 
 export default function Footer() {
+    const router = useRouter();
     return (
         <footer className={style.footer}>
             <div>
@@ -13,7 +15,7 @@ export default function Footer() {
 
             <div>
                 <h2>Precisa de ajuda?</h2>
-                <button><FaWhatsapp/>FALE COM A GENTE</button>
+                <button onClick={() => router.push("https://api.whatsapp.com/send/?phone=5521974806903&text&app_absent=0")}><FaWhatsapp />FALE COM A GENTE</button>
                 <p>Fale com a gente no whatsapp. Nossa equipe de atendimento está disponível para te atender de segunda a sexta, de 9 ás 18H</p>
                 <span>Desenvolvido por <Link href="https://api.whatsapp.com/send/?phone=5521982436773&text&app_absent=0" passHref>Conect-se</Link></span>
             </div>
